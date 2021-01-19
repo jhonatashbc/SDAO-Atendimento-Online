9908107a4e4838c3c341f4962d5eb5d930c66947
LimaSoftware <jhonatashbc@hotmail.com>
2021-01-19 15:50:32 -0300

    Inclusão do Service de atendimento e a exceção NotFoundException

---
+++ src/main/java/sistema/atendimento/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package sistema.atendimento.exception;
+
+public class NotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public NotFoundException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/sistema/atendimento/service/AtendenteService.java
@@ -0,0 +1,37 @@
+package sistema.atendimento.service;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sistema.atendimento.domain.Atendente;
+import sistema.atendimento.exception.NotFoundException;
+import sistema.atendimento.repository.AtendenteRepository;
+
+@Service
+public class AtendenteService {
+
+	@Autowired
+	private AtendenteRepository atendenteRepository;
+
+	public Atendente save(Atendente atendente) {
+		atendente.setCriacao(new Date());
+		atendente.setDisponivel(true);
+
+		Atendente createdAtendente = atendenteRepository.save(atendente);
+		return createdAtendente;
+	}
+	
+	public Atendente update(Atendente atendente) {
+		Atendente updatedAtendente = atendenteRepository.save(atendente);
+		return updatedAtendente;
+	}
+	
+	public Atendente getById(Long id){
+		Optional<Atendente> result = atendenteRepository.findById(id);
+		return result.orElseThrow(()-> new NotFoundException("Não existe nenhum atendente com o código = " + id));
+	}
+	
+}
